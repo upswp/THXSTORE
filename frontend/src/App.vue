d3e0906d78b3870227f4ab67ecc9c963624c46cf
daep93 <eoy1313@gmail.com>
2021-04-21 17:52:45 +0900

    [S04P31B202-28] feat : 프론트엔드 환경 구성

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      key: 0,
+    };
+  },
+  watch: {
+    key(newValue, oldValue) {
+      return newValue;
+    },
+  },
+
+  created() {},
+};
+</script>
+<style lang="scss">
+#app {
+  min-height: 100vh;
+}
+</style>
