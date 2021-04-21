848dfe3389461c36286e9b9484d7fa61e3d073d0
youngjoon_multicampus <junjun949567@gmail.com>
2021-04-21 17:30:33 +0900

    multimodule 개발

---
+++ backend/module-web/src/main/java/com/ssafy/thxstore/controller/ModuleWebApplication.java
@@ -0,0 +1,18 @@
+package com.ssafy.thxstore.controller;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+@SpringBootApplication
+public class ModuleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleWebApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+}
