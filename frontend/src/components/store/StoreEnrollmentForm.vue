aad946d45206047996bfb783b4b3cf4e6d1cfd14
gluwein <tjdwjddnr296@naver.com>
2021-04-27 17:16:07 +0900

    [S04P31B202-167] refactor: 다음 우편번호 찾기 API를 ifram에 연결시켜 모바일 사용성 고려

--- frontend/src/components/store/StoreEnrollmentForm.vue
+++ frontend/src/components/store/StoreEnrollmentForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="StoreEnrollment-container">
+  <div class="store-enrollment-container">
     <header><h1>스토어 정보입력</h1></header>
     <div>
       <form @submit.prevent>
@@ -15,6 +15,18 @@
             <div class="input-content">
               <input id="nomalAddress" v-model="nomalAddress" type="text" placeholder="주소" />
               <button id="addressButton" @click="getAddress">주소 찾기</button>
+              <div
+                id="layer"
+                style="display: none; position: fixed; overflow: hidden; z-index: 1; -webkit-overflow-scrolling: touch"
+              >
+                <img
+                  id="btnCloseLayer"
+                  src="//t1.daumcdn.net/postcode/resource/images/close.png"
+                  style="cursor: pointer; position: absolute; right: -3px; top: -3px; z-index: 1"
+                  alt="닫기 버튼"
+                  @click="closeDaumPostcode"
+                />
+              </div>
               <input id="detailAddress" v-model="detailAddress" type="text" placeholder="상세주소" />
             </div>
           </li>
@@ -26,7 +38,7 @@
                 type="tel"
                 name="phone"
                 pattern="[0,1]{3}-[0-9]{4}-[0-9]{4}"
-                placeholder="010-7777-7777 형식으로 입력해 주세요"
+                placeholder="010-○○○○-○○○○"
               />
             </div>
           </li>
@@ -38,16 +50,20 @@
                 type="tel"
                 name="phone"
                 pattern="[0-9]{3}-[0-9]{2}-[0-9]{5}"
-                placeholder="○○○-○○-○○○○○ 형식으로 입력해주세요!"
+                placeholder="○○○-○○-○○○○○"
               />
             </div>
           </li>
           <li>
             <label for="">사업자등록사본</label>
             <div class="input-content">
-              <input type="file" accept=".png, .jpg, .jpeg" />
+              <input id="fileButton" type="file" accept=".png, .jpg, .jpeg" />
             </div>
-            <input type="submit" />
+            <div class="submit-button">
+              <button type="submit">
+                <b>신청 완료</b>
+              </button>
+            </div>
           </li>
         </ul>
       </form>
@@ -68,7 +84,29 @@ export default {
     };
   },
   methods: {
+    initLayerPosition() {
+      var element_layer = document.getElementById('layer');
+      var width = 400; //우편번호서비스가 들어갈 element의 width
+      var height = 500; //우편번호서비스가 들어갈 element의 height
+      var borderWidth = 3; //샘플에서 사용하는 border의 두께
+
+      // 위에서 선언한 값들을 실제 element에 넣는다.
+      element_layer.style.width = width + 'px';
+      element_layer.style.height = height + 'px';
+      element_layer.style.border = borderWidth + 'px solid';
+      // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
+      element_layer.style.left =
+        ((window.innerWidth || document.documentElement.clientWidth) - width) / 2 - borderWidth + 60 + 'px';
+      element_layer.style.top =
+        ((window.innerHeight || document.documentElement.clientHeight) - height) / 2 - borderWidth + 'px';
+    },
+    closeDaumPostcode() {
+      // iframe을 넣은 element를 안보이게 한다.
+      var element_layer = document.getElementById('layer');
+      element_layer.style.display = 'none';
+    },
     getAddress() {
+      var element_layer = document.getElementById('layer');
       new window.daum.Postcode({
         oncomplete: data => {
           // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
@@ -91,46 +129,75 @@ export default {
           this.nomalAddress = addr;
           // 커서를 상세주소 필드로 이동한다.
           document.getElementById('detailAddress').focus();
+          element_layer.style.display = 'none';
         },
-      }).open();
+        width: '100%',
+        height: '100%',
+        maxSuggestItems: 5,
+      }).embed(element_layer);
+      element_layer.style.display = 'block';
+      this.initLayerPosition();
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.StoreEnrollment-container {
+.store-enrollment-container {
   padding: 5%;
-
   width: 100%;
 
   form {
     background-color: white;
     padding: 2%;
+    margin: auto;
+    max-width: 1024px;
   }
 
   header {
     @include flexbox;
     margin: 0% 5% 2% 5%;
-    justify-content: space-between;
+    justify-content: center;
   }
 
   label {
-    @include body2;
-    width: 15%;
+    @include body1;
+    width: 125px;
     margin-left: 3%;
     padding: 3px 5px 7px;
     float: left;
-    &::after {
-      content: '*';
-      color: red;
-    }
   }
   #nomalAddress {
     width: 40%;
   }
   .input-content {
     padding-left: 25%;
+  }
+  #fileButton {
+    padding: 0.5em 1em;
+    margin: 0 0.15em 0.7em 0;
+    border: 1px solid #ccc;
+    border-color: #dbdbdb #d2d2d2 #b2b2b2 #d2d2d3;
+    cursor: pointer;
+    color: #464646;
+    border-radius: 0.2em;
+    // vertical-align: middle;
+    font-size: 1em;
+    line-height: 1.25em;
+    background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#f2f2f2));
+  }
+  #addressButton {
+    padding: 0.5em 1em;
+    margin: 0.4em 0.15em 0.7em 0.15em;
+    border: 1px solid #ccc;
+    border-color: #dbdbdb #d2d2d2 #b2b2b2 #d2d2d3;
+    cursor: pointer;
+    color: #464646;
+    border-radius: 0.2em;
+    vertical-align: middle;
+    font-size: 1em;
+    line-height: 1.25em;
+    background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#f2f2f2));
   }
   input {
     display: inline-block;
@@ -140,6 +207,33 @@ export default {
     border: 2.5px solid #dfe1e6;
     padding: 10px 15px;
     border-radius: 3px;
+  }
+  .submit-button {
+    @include flexbox;
+    @include justify-content(flex-end);
+    button {
+      @include box-shadow;
+      background-color: $blue400;
+      width: 30%;
+      color: $white;
+      border: none;
+      &:hover:enabled {
+        background-color: $blue600;
+      }
+    }
+  }
+  @include mobile {
+    .StoreEnrollment-container {
+      padding: 1px;
+    }
+    label {
+      width: 100%;
+      float: left;
+    }
+    .input-content {
+      width: 90%;
+      padding: 1%;
+    }
   }
 }
 </style>
