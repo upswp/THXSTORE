f26cf8764300b8af8710d3dc77b5dda48abaa44f
gluwein <tjdwjddnr296@naver.com>
2021-04-29 10:53:25 +0900

    [S04P31B202-169] refactor: 애니메이션 효과, 디자인 수정

--- frontend/src/components/store/StoreEnrollmentForm.vue
+++ frontend/src/components/store/StoreEnrollmentForm.vue
@@ -11,22 +11,11 @@
             </div>
           </li>
           <li>
+            <set-road-name v-if="loaded" @newAddress="setLocationByRoadName">스토어 주소 등록</set-road-name>
             <label for="">스토어 주소</label>
             <div class="input-content">
               <input id="nomalAddress" v-model="nomalAddress" type="text" placeholder="주소" />
-              <button id="addressButton" @click="getAddress">주소 찾기</button>
-              <div
-                id="layer"
-                style="display: none; position: fixed; overflow: hidden; z-index: 1; -webkit-overflow-scrolling: touch"
-              >
-                <img
-                  id="btnCloseLayer"
-                  src="//t1.daumcdn.net/postcode/resource/images/close.png"
-                  style="cursor: pointer; position: absolute; right: -3px; top: -3px; z-index: 1"
-                  alt="닫기 버튼"
-                  @click="closeDaumPostcode"
-                />
-              </div>
+              <button id="addressButton" @click="loaded = true">주소 찾기</button>
               <input id="detailAddress" v-model="detailAddress" type="text" placeholder="상세주소" />
             </div>
           </li>
@@ -57,7 +46,15 @@
           <li>
             <label for="">사업자등록사본</label>
             <div class="input-content">
-              <input id="fileButton" type="file" accept=".png, .jpg, .jpeg" />
+              <div class="file-flex">
+                <input id="fileName" type="text" class="file_input_textbox" readonly />
+                <div class="file_input_div">
+                  <label for="file_1">
+                    <awesome id="faCloud" ref="cloud" icon="cloud-upload-alt" class="before-upload"></awesome>
+                  </label>
+                  <input id="file_1" type="file" name="file_1" class="file_input_hidden" @change="insertedFile" />
+                </div>
+              </div>
             </div>
             <div class="submit-button">
               <button type="submit">
@@ -72,7 +69,11 @@
 </template>
 
 <script>
+import SetRoadName from '@/components/common/SetRoadName.vue';
 export default {
+  components: {
+    SetRoadName,
+  },
   data() {
     return {
       storeName: '',
@@ -81,73 +82,22 @@ export default {
       detailAddress: '',
       phoneNum: '',
       comResNum: '',
+      loaded: false,
+      file: '',
     };
   },
   methods: {
-    initLayerPosition() {
-      var element_layer = document.getElementById('layer');
-      var width = 400; //우편번호서비스가 들어갈 element의 width
-      var height = 500; //우편번호서비스가 들어갈 element의 height
-      var borderWidth = 3; //샘플에서 사용하는 border의 두께
-
-      // 위에서 선언한 값들을 실제 element에 넣는다.
-      element_layer.style.width = width + 'px';
-      element_layer.style.height = height + 'px';
-      element_layer.style.border = borderWidth + 'px solid';
-      // 실행되는 순간의 화면 너비와 높이 값을 가져와서 중앙에 뜰 수 있도록 위치를 계산한다.
-
-      if (window.innerWidth > 728) {
-        element_layer.style.left =
-          ((window.innerWidth || document.documentElement.clientWidth) - width) / 2 - borderWidth + 180 + 'px';
-        element_layer.style.top =
-          ((window.innerHeight || document.documentElement.clientHeight) - height) / 2 - borderWidth + 'px';
-      } else {
-        // 모바일의 경우 주소찾기 창 크기를 조절한다.
-        var width = 300;
-        element_layer.style.width = width + 'px';
-        element_layer.style.left =
-          ((window.innerWidth || document.documentElement.clientWidth) - width) / 2 - borderWidth + 'px';
-        element_layer.style.top =
-          ((window.innerHeight || document.documentElement.clientHeight) - height) / 2 - borderWidth + 'px';
+    insertedFile(event) {
+      const file = event.target.files[0];
+      const fileValue = event.target.value;
+      document.getElementById('fileName').value = fileValue;
+      if (file) {
+        this.$refs.cloud.classList.add('after-upload');
       }
     },
-    closeDaumPostcode() {
-      // iframe을 넣은 element를 안보이게 한다.
-      var element_layer = document.getElementById('layer');
-      element_layer.style.display = 'none';
-    },
-    getAddress() {
-      var element_layer = document.getElementById('layer');
-      new window.daum.Postcode({
-        oncomplete: data => {
-          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
-
-          // 각 주소의 노출 규칙에 따라 주소를 조합한다.
-          // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-          var addr = ''; // 주소 변수
-
-          //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
-          if (data.userSelectedType === 'R') {
-            // 사용자가 도로명 주소를 선택했을 경우
-            addr = data.roadAddress;
-          } else {
-            // 사용자가 지번 주소를 선택했을 경우(J)
-            addr = data.jibunAddress;
-          }
-
-          // 우편번호와 주소 정보를 해당 필드에 넣는다.
-          this.zip = data.zonecode;
-          this.nomalAddress = addr;
-          // 커서를 상세주소 필드로 이동한다.
-          document.getElementById('detailAddress').focus();
-          element_layer.style.display = 'none';
-        },
-        width: '100%',
-        height: '100%',
-        maxSuggestItems: 5,
-      }).embed(element_layer);
-      element_layer.style.display = 'block';
-      this.initLayerPosition();
+    setLocationByRoadName(data) {
+      this.loaded = false;
+      this.nomalAddress = data;
     },
   },
 };
@@ -163,6 +113,7 @@ export default {
     padding: 2%;
     margin: auto;
     max-width: 1024px;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
   }
 
   header {
@@ -178,25 +129,72 @@ export default {
     padding: 3px 5px 7px;
     float: left;
   }
+  input {
+    display: inline-block;
+    width: 100%;
+    margin-bottom: 10px;
+    background-color: none;
+    border: 2.5px solid #dfe1e6;
+    padding: 10px 15px;
+    border-radius: 3px;
+    z-index: 0;
+  }
   #nomalAddress {
-    width: 40%;
+    width: 60%;
   }
   .input-content {
     padding-left: 25%;
   }
-  #fileButton {
-    padding: 0.5em 1em;
-    margin: 0 0.15em 0.7em 0;
-    border: 1px solid #ccc;
-    border-color: #dbdbdb #d2d2d2 #b2b2b2 #d2d2d3;
-    cursor: pointer;
-    color: #464646;
-    border-radius: 0.2em;
-    // vertical-align: middle;
-    font-size: 1em;
-    line-height: 1.25em;
-    background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#f2f2f2));
+
+  .file-flex {
+    @include flexbox;
+    background-color: none;
+    // border: 2.5px solid #dfe1e6;
   }
+
+  .file_input_textbox {
+    display: inline-block;
+    width: 90%;
+    margin-bottom: 0px;
+    margin-right: 1%;
+    padding: 1px 1px;
+    background-color: none;
+    border: 2.5px solid #dfe1e6;
+  }
+  .file_input_div {
+    position: relative;
+    width: 70px;
+    height: 36px;
+    overflow: hidden;
+  }
+  .file_input_img_btn {
+    padding: 0 0 0 5px;
+  }
+  .file_input_hidden {
+    font-size: 20px;
+    padding: 0;
+    position: absolute;
+    right: 0px;
+    top: 0px;
+    opacity: 0;
+    filter: alpha(opacity=0);
+    -ms-filter: alpha(opacity=0);
+    cursor: pointer;
+  }
+  .before-upload {
+    color: $blue400;
+    font-size: 40px;
+    cursor: pointer;
+  }
+  input::-webkit-file-upload-button {
+    cursor: pointer;
+  }
+
+  .after-upload {
+    transition: color 0.3s;
+    color: $blue600;
+  }
+
   #addressButton {
     padding: 0.5em 1em;
     margin: 0.4em 0.15em 0.7em 0.15em;
@@ -210,15 +208,7 @@ export default {
     line-height: 1.25em;
     background-image: -webkit-gradient(linear, left top, left bottom, from(#fff), to(#f2f2f2));
   }
-  input {
-    display: inline-block;
-    width: 100%;
-    margin-bottom: 10px;
-    background-color: $gray000;
-    border: 2.5px solid #dfe1e6;
-    padding: 10px 15px;
-    border-radius: 3px;
-  }
+
   .submit-button {
     @include flexbox;
     @include justify-content(flex-end);
