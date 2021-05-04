a449aa9542f9af41a44cc66673173bea0fb01f5d
Park Sangwoo <tkddnsos@gmail.com>
2021-05-05 01:55:40 +0900

    [S04P31B202-197] refactor : Spring security CORS Troubleshooting

--- backend/module-web/src/main/java/com/ssafy/thxstore/controller/security/LoginSuccessHandler.java
+++ backend/module-web/src/main/java/com/ssafy/thxstore/controller/security/LoginSuccessHandler.java
@@ -7,14 +7,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@CrossOrigin(origins = { "*" }, maxAge = 6000)
+
 @RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	private final JwtTokenProvider jwtTokenProvider;
