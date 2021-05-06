09a76efe5b9b18171113f87d1a8c815197e24ea6
Choi DongSik <opert1564@naver.com>
2021-05-07 00:32:54 +0900

    [S04P31B202-91] feat : storeApplication 0.1.0 version

--- backend/module-api/src/main/java/com/ssafy/thxstore/store/service/StoreService.java
+++ backend/module-api/src/main/java/com/ssafy/thxstore/store/service/StoreService.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
+// todo 필요없는 주석 처리.
 @Service
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PUBLIC)
@@ -39,14 +40,17 @@ public class StoreService {
     private final ImageService imageService;
 
     // 스토어 생성
-    public Store createStore(String imgProfile, CreateStoreFileDto createStoreFileDto) {
-        Member member = memberRepository.findById(createStoreFileDto.getMemberId()).get();
-        member.setRole(MemberRole.MANAGER);
+    // todo error 예외 처리 필요
+    @Transactional
+    public Store createStore(String imgProfile, CreateStoreFileDto createStoreFileDto, String email) {
+//        Member member = memberRepository.findById(createStoreFileDto.getMemberId()).get();
+        Member member = memberRepository.findByEmail(email).get();
+//        member.setRole(MemberRole.MANAGER);
 
         // 파일 저장
         CreateStoreDto createStoreDto = CreateStoreDto.builder()
                 .member(member)
-                .memberId(createStoreFileDto.getMemberId())
+                .memberId(member.getId())
                 .name(createStoreFileDto.getName())
                 .mainAddress(createStoreFileDto.getMainAddress())
                 .subAddress(createStoreFileDto.getSubAddress())
@@ -66,11 +70,15 @@ public class StoreService {
 
     /*판매자 스토어 페이지(스토어 정보 입력)*/
     // 스토어 정보 가져오기
-    public Optional<Store> getStore(Long storeId) {
-        Optional<Store> store =  storeRepository.findById(storeId);
+    public Optional<Store> getStore(String email) {
+        Member member = memberRepository.findByEmail(email).get();
+
+        Optional<Store> store = storeRepository.findByMemberId(member.getId());
+       // Optional<Store> store =  storeRepository.findById(storeId);
         return store;
     }
 
+    // todo 수정 부분 -> 금요일이나 다음 주 작업
     // 스토어 개인정보 변경 개인
     public Store patchStore(StoreChangedDto storeChangedDto){
         Store store = storeRepository.findById(storeChangedDto.getStoreId()).get();
@@ -132,15 +140,17 @@ public class StoreService {
         return store;
     }
 
-    // 스토어 신청 허가. 영속성 ㄲ
+    // 스토어 신청 허가.
     @Transactional
-    public Store storeApplicationSuccess(Long storeId){
+    public Store storeApplicationSuccess(Long storeId,String email){
         Store store = storeRepository.findById(storeId).get();
+        Member member = memberRepository.findByEmail(email).get();
+        member.setRole(MemberRole.MANAGER);
         //store = Store.builder()
        //         .checkStore(CheckStore.EDIT_WAITING)
        //         .build();
         store.setCheckStore(CheckStore.NORMAL);
-        System.out.println(store.getCheckStore());
+        //System.out.println(store.getCheckStore());
         return store;
     }
     // 스토어 신청 불가 여기서는 store status만 변경
@@ -151,13 +161,18 @@ public class StoreService {
     }
     // 스토어 신청 불가 확인 -> 매니저 -> 유저,  스토어 삭제
     @Transactional
-    public void storeApplicationConfirm(Long storeId){
-        Store store = storeRepository.findById(storeId).get();
-        Long memberId = store.getMember().getId();
+    public void storeApplicationConfirm(String email){
 
-        Member member = memberRepository.findById(memberId).get();
-        member.setRole(MemberRole.USER);
-        storeRepository.deleteById(storeId);
+        Member member = memberRepository.findByEmail(email).get();
+        Store store = storeRepository.findByMemberId(member.getId()).get();
+        storeRepository.deleteById(store.getId());
+
+//        Store store = storeRepository.findById(storeId).get();
+//        Long memberId = store.getMember().getId();
+//
+//        Member member = memberRepository.findById(memberId).get();
+//        member.setRole(MemberRole.USER);
+//        storeRepository.deleteById(storeId);
     }
     // 스토어 수정 리스트
 
@@ -168,7 +183,6 @@ public class StoreService {
         return false;
     }
 
-    //tes
     public Store createStoreTest(CreateStoreDto createStoreDto) {
         Store store = modelMapper.map(createStoreDto, Store.class);
 
@@ -176,7 +190,6 @@ public class StoreService {
 
         return store;
     }
-
 
     public List<Store> storeModifyList() {
         List<Store> store = null;//storeRepository.findByStoreCategory("EDIT_WAITING");
