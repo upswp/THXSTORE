f55985f3920a790740ba790015e602f64a406c0f
Park Sangwoo <tkddnsos@gmail.com>
2021-05-09 07:16:14 +0900

    [S04P31B202-222] refactor : if login, return member data feature

--- backend/module-web/src/main/java/com/ssafy/thxstore/controller/security/LoginSuccessHandler.java
+++ backend/module-web/src/main/java/com/ssafy/thxstore/controller/security/LoginSuccessHandler.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.thxstore.controller.config.JwtTokenProvider;
 import com.ssafy.thxstore.member.dto.response.AuthResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -17,12 +19,19 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 	private final JwtTokenProvider jwtTokenProvider;
+	private Logger logger = LoggerFactory.getLogger(LoginSuccessHandler.class);
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+		UserPrincipal  loginResponse = (UserPrincipal) authentication.getPrincipal();
+		logger.info(String.valueOf(loginResponse));
 		response.setStatus(HttpStatus.OK.value());
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
-		new ObjectMapper().writeValue(response.getWriter(), new AuthResponse(jwtTokenProvider.createToken(authentication)));
+		new ObjectMapper().writeValue(response.getWriter(),
+				new AuthResponse(
+						jwtTokenProvider.createToken(authentication),
+						loginResponse.getUser()
+				));
 	}
 }
