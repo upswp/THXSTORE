6792bc92cbc016d8acc7bdfeee264d0571f3b440
daep93 <eoy1313@gmail.com>
2021-05-10 09:33:18 +0900

    [S04P31B202-243] feat : 메인 메뉴 디자인 변경 및 로그아웃 추가

--- frontend/src/components/common/MainAside.vue
+++ frontend/src/components/common/MainAside.vue
@@ -26,6 +26,12 @@
           </span>
           <span class="detail-guide-text">관리자 대시보드</span>
         </div>
+        <div ref="admin-icon" class="drawer-item" @click="moveToPage('login')">
+          <span class="drawer-icon-wrapper">
+            <awesome icon="sign-out-alt"></awesome>
+          </span>
+          <span class="detail-guide-text">로그아웃</span>
+        </div>
       </nav>
     </div>
   </transition>
@@ -48,5 +54,77 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/sample';
+.slide-vertical-enter-active,
+.slide-vertical-leave-active {
+  transition-duration: 0.5s;
+  transition-property: height, opacity, transform;
+  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
+  overflow: hidden;
+}
+.slide-vertical-leave-active,
+.slide-vertical-enter {
+  opacity: 0;
+  @include transform(translate(0, -10em));
+}
+
+.drawer-container {
+  position: fixed;
+  z-index: 2;
+  top: 50px;
+  right: 0px;
+  box-shadow: 0 10px 5px rgba(0, 0, 0, 0.2);
+  @include mobile() {
+    top: 44px;
+  }
+  @include xs-mobile() {
+    width: 100%;
+    top: 44px;
+  }
+}
+.navigation-icon-wrapper {
+  @include transition(all 0.5s);
+  // header 크기가 변함에 따라 top 변경해줘야함.
+  width: 100%;
+  font-size: 1.4rem;
+  background-color: white;
+  border: none;
+}
+.drawer-icon-wrapper {
+  display: inline-block;
+  width: 35px;
+  svg {
+    width: 25px;
+    height: 25px;
+    @include xs-mobile() {
+      width: 22px;
+      height: 22px;
+    }
+  }
+}
+.drawer-item {
+  @include flexbox;
+  @include align-items(center);
+  @include transition(all 0.5s);
+  color: black;
+  padding: 10px;
+  cursor: pointer;
+  border: 1px solid white;
+  border-bottom: 1px solid $gray600;
+  &:hover {
+    color: white;
+    background-color: $blue600;
+    border: 1px solid $blue600;
+  }
+  @include pc() {
+    padding: 5px;
+  }
+  @include mobile() {
+    padding: 5px;
+    font-size: 1.1rem;
+  }
+  @include xs-mobile() {
+    padding: 5px;
+    font-size: 1rem;
+  }
+}
 </style>
