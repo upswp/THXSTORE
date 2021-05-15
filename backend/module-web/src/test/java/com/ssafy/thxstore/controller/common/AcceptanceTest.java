79b4a447d0bb1e4c5252e81e80659a2dca0f7b74
Park Sangwoo <tkddnsos@gmail.com>
2021-05-15 21:36:41 +0900

    [S04P31B202-313] refactor : io.rest-assured comment 처리

--- backend/module-web/src/test/java/com/ssafy/thxstore/controller/common/AcceptanceTest.java
+++ backend/module-web/src/test/java/com/ssafy/thxstore/controller/common/AcceptanceTest.java
@@ -1,24 +1,24 @@
-package com.ssafy.thxstore.controller.common;
-
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public abstract class AcceptanceTest extends BaseControllerTest{
-    @LocalServerPort
-    int port;
-
-    @Autowired
-    private AdminCreate adminCreate;
-
-    @BeforeEach
-    public void setUp() {
-        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
-            RestAssured.port = port;
-        }
-        adminCreate.execute();
-    }
-}
+//package com.ssafy.thxstore.controller.common;
+//
+//import io.restassured.RestAssured;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.web.server.LocalServerPort;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//public abstract class AcceptanceTest extends BaseControllerTest{
+//    @LocalServerPort
+//    int port;
+//
+//    @Autowired
+//    private AdminCreate adminCreate;
+//
+//    @BeforeEach
+//    public void setUp() {
+//        if (RestAssured.port == RestAssured.UNDEFINED_PORT) {
+//            RestAssured.port = port;
+//        }
+//        adminCreate.execute();
+//    }
+//}
