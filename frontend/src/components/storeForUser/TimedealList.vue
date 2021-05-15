d306edc8c258146dd001410e92bd2945b3137bff
gluwein <tjdwjddnr296@naver.com>
2021-05-16 03:16:06 +0900

    [S04P31B202-350] feat: 사용자를 위한 타임딜 HTML

--- frontend/src/components/storeForUser/TimedealList.vue
+++ frontend/src/components/storeForUser/TimedealList.vue
@@ -1,8 +1,42 @@
 <template>
-  <div>
-    여기는 스토어가 진행중인 타임딜이 들어갈 자리 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto
-    inventore omnis voluptatibus architecto alias expedita vel. Cumque non excepturi qui eaque et molestiae veritatis
-    ratione, quidem vitae aperiam expedita in.
+  <div class="timedeal-container">
+    <header><h3>타임딜 남은 시간 : 1시간</h3></header>
+    <br />
+    <br />
+    <div class="timedeal-item">
+      <div class="item-thumbnail">
+        <img
+          src="https://d20aeo683mqd6t.cloudfront.net/ko/articles/title_images/000/039/143/original/IMG_5649%E3%81%AE%E3%82%B3%E3%83%92%E3%82%9A%E3%83%BC.jpg?2019&d=750x400"
+          alt=""
+        />
+      </div>
+      <div class="item-info">
+        <div class="reserved-stock">50개 남음</div>
+        <div class="info-name">다꼬야끼세트</div>
+        <div class="info-introduce">
+          <p>
+            가쓰오부시, 데리야끼 소스, 머스터드 등을 뿌려주면 더욱 짭조름하고 맛있게 즐길 수 있답니다. 한입에 쏙
+            들어가는 앙증맞은 크기 속 진득한 감칠맛을 만나보세요.
+          </p>
+        </div>
+
+        <div class="info-footer">
+          <div class="info-timedeal-price">
+            <div class="info-origin-price"><s>8000원</s></div>
+            <!-- <div class="price-label">타임딜가</div> -->
+            <div class="info-sale-price"><b>4,000</b>원</div>
+          </div>
+          <div class="order-number-container">
+            <!-- <label for="order-number">수량 </label> -->
+            <input id="order-number" type="number" class="order-number" placeholder="숫자 " min="1" :max="100" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="timedeal-footer-group">
+      <div class="timedeal-sum">총합계는 요</div>
+      <button class="timedeal-sum">예약번호 받기</button>
+    </footer>
   </div>
 </template>
 
--- frontend/src/components/storeForUser/UserStoreMenuList.vue
+++ frontend/src/components/storeForUser/UserStoreMenuList.vue
@@ -60,5 +60,272 @@ export default {
 </script>
 
 <style scoped lang="scss">
-@import '@/assets/scss/sample2';
+// @import '@/assets/scss/sample2';
+.userstore-menu-containter {
+  @include flexbox;
+  flex-wrap: wrap;
+  .menu-group-container {
+    width: 100%;
+    border: 2px $gray200 solid;
+    box-shadow: 0 0 0 2px $gray200 inset;
+    margin-bottom: 10px;
+    .menu-group-title {
+      background-color: $gray100;
+      @include flexbox;
+      @include align-items(flex-start);
+      width: 100%;
+      padding: 1%;
+      font-size: 1.3rem;
+      cursor: pointer;
+
+      @include xs-mobile {
+        padding: 1.3%;
+      }
+      // border: 2px $blue400 solid;
+      .group-title {
+        width: 95%;
+        display: inline-block;
+        margin: auto;
+        font-weight: $bold;
+        @include xs-mobile {
+          font-size: 0.7em;
+          margin-left: 3px;
+          font-weight: $bold;
+        }
+      }
+      .chevron-up {
+        color: $gray600;
+        font-size: 1.2em;
+        margin-right: 1.4%;
+        &:hover {
+          cursor: pointer;
+          color: $gray800;
+        }
+        @include xs-mobile {
+          font-size: 0.8em;
+        }
+      }
+    }
+    @keyframes fade-in {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+
+    @keyframes fade-out {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0;
+      }
+    }
+    .transition-div {
+      animation: fade-in 1s;
+      animation-fill-mode: forwards;
+    }
+    .upside-down {
+      transform: rotate(180deg);
+    }
+    .hidden {
+      display: none;
+      animation: fade-out 1s;
+
+      // visibility: hidden;
+    }
+    .menu-list-container {
+      @include flexbox;
+      flex-wrap: wrap;
+      .menu-list-item {
+        border: 1px solid $gray200;
+        @include shadow3;
+        margin: 1%;
+        @include flexbox;
+        width: 48%;
+        padding: 1%;
+        flex-wrap: wrap;
+        @include xs-mobile {
+          width: 100%;
+        }
+      }
+
+      .menu-list-info {
+        width: 70%;
+        .info-name {
+          padding-bottom: 3%;
+          @include xs-mobile {
+            font-size: 0.8rem;
+          }
+        }
+        .info-content {
+          width: 90%;
+          font-size: 0.8em;
+          @include ellipsis;
+          // text-overflow: ellipsis; overflow: hidden;
+          // white-space: nowrap;
+          // -webkit-line-clamp:3; /* 라인수 */
+          // -webkit-box-orient:vertical;
+          // white-space:normal;
+          // line-height:1.2em;
+          // height:3.6em;
+          // overflow:hidden;
+          // text-align:left;
+          @include lg-pc {
+            height: 2.4em;
+          }
+          @include xs-mobile {
+            // min-height: 18%;
+          }
+        }
+        .info-price {
+          font-size: 0.9em;
+          @include xs-mobile {
+            font-size: 0.8em;
+          }
+        }
+        .info-degree {
+          font-size: 0.7em;
+        }
+      }
+      .menu-thumbnail {
+        width: 30%;
+        text-align: center;
+        img {
+          object-fit: contain;
+          border-radius: 5%;
+          @include lg-pc {
+            width: 130px;
+            height: 140px;
+          }
+          @include pc {
+            width: 95px;
+            height: 100px;
+          }
+          @include mobile {
+            width: 100px;
+            height: 105px;
+          }
+          @include xs-mobile {
+            width: 89px;
+            height: 95px;
+          }
+        }
+      }
+    }
+  }
+}
+
+.userstore-container {
+  @include flexbox;
+  flex-wrap: wrap;
+  // align-items: flex-start;
+  h2 {
+    @include xs-mobile {
+      font-size: 17px;
+    }
+  }
+  .nav-aside {
+    align-self: flex-start;
+    position: fixed;
+    text-align: center;
+    font-size: 1rem;
+    padding: 10px 0;
+    margin: 0;
+    width: 200px;
+    min-height: 100%;
+    box-shadow: 2px 0 5px rgba(0, 0, 0, 0.3);
+    div {
+      padding: 10px 0;
+      &:hover {
+        background: $blue400;
+        color: white;
+      }
+      &.active {
+        background: $blue400;
+        color: white;
+      }
+      @include mobile {
+        padding: 4px;
+        flex-basis: 25%;
+      }
+      @include xs-mobile {
+        padding: 2px;
+        flex-basis: 25%;
+        font-size: 11px;
+      }
+    }
+    &:hover {
+      cursor: pointer;
+    }
+    @include mobile {
+      // display: none;
+      @include flexbox;
+      @include justify-content(space-evenly);
+      padding: 5px;
+      width: 100%;
+      position: static;
+      min-height: 5px;
+      flex-grow: 0;
+    }
+    @include xs-mobile {
+      @include flexbox;
+      @include justify-content(space-evenly);
+      padding: 3px;
+      width: 100%;
+      position: static;
+      min-height: 5px;
+      flex-grow: 0;
+    }
+  }
+  .main-content {
+    width: calc(100% - 200px);
+    padding: 20px;
+    max-width: 800px;
+    margin-left: 200px;
+    @include mobile {
+      width: 100%;
+      margin: 0px;
+    }
+    @include xs-mobile {
+      margin: 0px;
+      width: 100%;
+    }
+  }
+  //   border: 2px blue solid;
+  .header-container {
+    .time-deal-ani {
+      font-family: neon;
+      color: #fb4264;
+      font-size: 2vw;
+      line-height: 3vw;
+      text-shadow: 0 0 2vw #f40a35;
+      animation: neon 1s ease infinite;
+      -moz-animation: neon 1s ease infinite;
+      -webkit-animation: neon 1s ease infinite;
+    }
+    @keyframes neon {
+      0%,
+      70% {
+        //   text-shadow: 0 0 1vw #FA1C16, 0 0 3vw #FA1C16, 0 0 10vw #FA1C16, 0 0 10vw #FA1C16, 0 0 .4vw #FED128, ;
+        color: #f40a35;
+      }
+      50% {
+        text-shadow: 0 0 0.5vw #800e0b, 0 0 1.5vw #800e0b, 0 0 5vw #800e0b, 0 0 5vw #800e0b;
+        //   color: #806914;
+      }
+    }
+    @include pc {
+    }
+    .header-thumbnail {
+      padding: 1%;
+      border: 2px $gray200 solid;
+    }
+  }
+  h2 {
+    text-align: center;
+  }
+}
 </style>
