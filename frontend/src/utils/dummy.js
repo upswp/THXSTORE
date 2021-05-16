a75ae4e1149937cf834e95a45e7b57bcc65272aa
daep93 <eoy1313@gmail.com>
2021-05-17 00:57:30 +0900

    [S04P31B202-356] feat : 네비게이션 가드 적용

---
+++ frontend/src/utils/dummy.js
@@ -0,0 +1,8 @@
+const dummy = [
+  {
+    id: 1,
+    name: '대현 스토어',
+    storeCatogry: 'DEFAULT',
+  },
+];
+export {};
---
+++ frontend/src/views/TimeDealFeedPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="feed-container"></div>
+</template>
+
+<script>
+import { dummy } from '@/utils/dummy';
+export default {
+  data() {
+    return {
+      timeDealList: [{}],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
