9ab5813a79c807ef40fc04a300703ddff5d5ecdb
junjun0905 <junjun949567@gmail.com>
2021-05-18 01:34:16 +0900

    [S04P31B202-359] feat : 맴버가 주문 조회 시 주문 최신순으로 조회

--- backend/module-common/src/main/java/com/ssafy/thxstore/reservation/dto/ReservationDto.java
+++ backend/module-common/src/main/java/com/ssafy/thxstore/reservation/dto/ReservationDto.java
@@ -5,15 +5,19 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class ReservationDto {
+public class ReservationDto implements Comparable<ReservationDto>{
 //    @NotBlank
     private Long userId;
+
+    private Long reservationId;
 
     private String nickname;
 
@@ -36,9 +40,10 @@ public class ReservationDto {
     private List<ReservationGroupDto> reservationGroups;
 
     @Builder
-    public ReservationDto(String storeImg,String storeName,String email,String nickname,String orderTime,Long storeId, Long userId, List<ReservationGroupDto> reservationGroups, ReservationStatus reservationStatus) {
+    public ReservationDto(Long reservationId,String storeImg,String storeName,String email,String nickname,String orderTime,Long storeId, Long userId, List<ReservationGroupDto> reservationGroups, ReservationStatus reservationStatus) {
         this.userId = userId;
         this.email = email;
+        this.reservationId = reservationId;
         this.storeImg = storeImg;
         this.storeName =storeName;
         this.nickname =nickname;
@@ -50,5 +55,14 @@ public class ReservationDto {
 
     public void updateOrderTime(String orderTime) {
         this.orderTime = orderTime;
+    }
+
+    @Override
+    public int compareTo(ReservationDto arg0) {
+        // TODO Auto-generated method stub
+        Long target = arg0.getReservationId();
+        if(reservationId == target) return 0;
+        else if(reservationId > target) return 1;
+        else return -1;
     }
 }
