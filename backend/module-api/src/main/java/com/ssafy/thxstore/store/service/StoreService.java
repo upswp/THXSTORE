1d0414520b4d6ac2cab113207bb451be741d9679
Choi DongSik <opert1564@naver.com>
2021-05-18 02:08:29 +0900

    [S04P31B202-257] refactor : 타임딜 시간 재변경

--- backend/module-api/src/main/java/com/ssafy/thxstore/store/service/StoreService.java
+++ backend/module-api/src/main/java/com/ssafy/thxstore/store/service/StoreService.java
@@ -442,8 +442,8 @@ public class StoreService {
             }
             // 2시간 전이란 것을 비교해야함 date1과 date2
             // 현재 시간과 date2시간이 2시간 넘게 차이남다면(+로)
-           // if(date1.getTime() - date2.getTime() > 7200000){ // 2시간
-            if(date1.getTime() - date2.getTime() > 600000){
+           // if(date1.getTime() - date2.getTime() > 7200000){ // 2시간, 10분 600000, 4시간 14400000, 하루 86400000
+            if(date1.getTime() - date2.getTime() > 14400000){
                 Product product = productRepository.findById(timeDealList.get(i).getProduct().getId()).get();
                 product.setStock(null);
                 product.setRate(null);
