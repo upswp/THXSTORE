f438b6678460f70924d8e2824bc5f6b2bd1260c4
junjun0905 <junjun949567@gmail.com>
2021-05-19 00:17:27 +0900

    [S04P31B202-391] fix : 테스트 코드 수정

--- backend/module-web/src/test/java/com/ssafy/thxstore/controller/order/docs/OrderDocumentation.java
+++ backend/module-web/src/test/java/com/ssafy/thxstore/controller/order/docs/OrderDocumentation.java
@@ -34,7 +34,8 @@ public class OrderDocumentation {
                         fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("리뷰를 작성할 아이디"),
                         fieldWithPath("reservationId").type(JsonFieldType.NUMBER).description("리뷰가 달릴 주문 번호"),
                         fieldWithPath("star").type(JsonFieldType.NUMBER).description("별점"),
-                        fieldWithPath("dateTime").type(JsonFieldType.STRING).description("리뷰 작성 시간")
+                        fieldWithPath("dateTime").type(JsonFieldType.STRING).description("리뷰 작성 시간"),
+                        fieldWithPath("logo").type(JsonFieldType.STRING).description("로고 이미지 주소")
                 ),
                 responseHeaders(
                         headerWithName(HttpHeaders.LOCATION).description("Location header"),
