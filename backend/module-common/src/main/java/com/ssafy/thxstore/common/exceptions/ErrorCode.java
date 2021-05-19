ca51cfb6b34a090ad0240762dba77c39dd2101fb
junjun0905 <junjun949567@gmail.com>
2021-05-19 19:48:13 +0900

    [S04P31B202-357] fix : 주문 재고 오류 반환

--- backend/module-common/src/main/java/com/ssafy/thxstore/common/exceptions/ErrorCode.java
+++ backend/module-common/src/main/java/com/ssafy/thxstore/common/exceptions/ErrorCode.java
@@ -1,9 +1,13 @@
 package com.ssafy.thxstore.common.exceptions;
 
+import com.ssafy.thxstore.product.dto.ProductDto;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public enum ErrorCode {
+
     INTERNAL_SERVER_ERROR(500, "C_001", "서버가 터졌습니다."),
     METHOD_NOT_ALLOWED(405, "C_002", "Api는 열려있으나 메소드는 사용 불가합니다."),
     INVALID_INPUT_VALUE(400, "C_003", "적절하지 않은 요청 값입니다."),
@@ -19,10 +23,11 @@ public enum ErrorCode {
     BAD_LOGIN(400, "AU_005", "잘못된 아이디 또는 패스워드입니다."),
     UNAUTHORIZED_MEMBER(400,"AU_006","존재하지않는 유저입니다."),
     CHECK_REVIEW(200,"AU_008","이미 작성된 리뷰가 있습니다."),
+    OUT_OF_STOCK(400,"AU_008","재고가 떨어졌습니다."),
     NOT_FOUND_IMAGE(400,"AU_007","이미지 관련 오류가 발생했습니다.");
 
     //-----------------------
-
+//    private static List<ProductDto> productlist;
     private final String code;
     private final String message;
     private final int status;
@@ -32,4 +37,11 @@ public enum ErrorCode {
         this.message = message;
         this.code = code;
     }
+
+//    ErrorCode(int status, String code, String message) {
+//        this.status = status;
+//        this.message = message;
+//        this.code = code;
+////        this.productlist = productlist;
+//    }
 }
